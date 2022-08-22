e6cb2536d49eb2ba486dfef0cde836c087c4f98b
antoninhrlt <antoninhrlt1@gmail.com>
2022-08-22 13:11:43 +0200

    Compile the src/x11.c file with the wm

--- build/wm.mk
+++ build/wm.mk
@@ -11,7 +11,7 @@ CC_FLAGS = $(CC_DEBUG_FLAGS) -I inc/wm -I inc/
 OUT_DIR = build/out/wm
 
 SRC = $(shell find src/wm -name '*.c')
-OBJ = $(patsubst src/wm/%.c, $(OUT_DIR)/%.o,$(SRC))
+OBJ = $(patsubst src/wm/%.c, $(OUT_DIR)/%.o,$(SRC)) $(OUT_DIR)/x11.o
 
 BIN = build/out/wm.mira
 
@@ -24,4 +24,7 @@ $(BIN) : $(OBJ)
 	$(CC) $(CC_DEPS) $(CC_FLAGS) -o $@ $^
 
 $(OUT_DIR)/%.o : src/wm/%.c
+	$(CC) $(CC_DEPS) $(CC_FLAGS) -c -o $@ $^
+
+$(OUT_DIR)/x11.o : src/x11.c
 	$(CC) $(CC_DEPS) $(CC_FLAGS) -c -o $@ $^
