5cdbad3495bcaf0cc78cb8bd5462ea99236bf80b
antoninhrlt <antoninhrlt1@gmail.com>
2022-08-22 19:53:57 +0200

    Add tiling enumeration

---
+++ inc/wm/tiling.h
@@ -0,0 +1,15 @@
+// This file is part of "mira"
+// Under the MIT License
+// Copyright (c) 2022 Antonin Hérault
+
+#ifndef MIRA_TILING
+#define MIRA_TILING
+
+typedef enum {
+    Full,
+    LeftHalf,
+    RightHalf,
+    BottomHalf,
+} TilingMode;
+
+#endif // MIRA_TILING
